cb4004485f99b009e200ef2dda0e151cd7cbf79e
Aayush Vijayvergiya <aayush.vijayvergiya93@gmail.com>
2024-07-18 19:39:54 +0530

    Initial commit

---
+++ config/ci.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleClientID:
+    "76410639016-m26jarbh56pep795jf9vti47mtob0b28.apps.googleusercontent.com",
+  googleClientSecret: "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",
+  mongoURI: "mongodb://127.0.0.1:27017/blog_ci",
+  cookieKey: "123123123",
+  redisUrl: "redis://127.0.0.1:6379",
+};
---
+++ config/dev.js
@@ -0,0 +1,9 @@
+module.exports = {
+  googleClientID:
+    "76410639016-m26jarbh56pep795jf9vti47mtob0b28.apps.googleusercontent.com",
+  googleClientSecret: "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",
+  mongoURI:
+    "mongodb+srv://aayushvijayvergiya2006:nextjs-events-app@nextjs-events-app-clust.xyp32w3.mongodb.net/?retryWrites=true&w=majority",
+  cookieKey: "123123123",
+  redisUrl: "redis://127.0.0.1:6379",
+};
